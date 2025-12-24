68c595c2db00dc72da8664f248b02e97cd836fdf
RabinOsti12 <rosti@nepops.com>
2025-12-24 11:00:38 +0545

    refact

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function profile() {
+  // console.log("hello")
   return (
     <View style={styles.container}>
       <Text>This is profile page</Text>
